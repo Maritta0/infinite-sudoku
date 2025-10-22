8dcb9162c1f549c4f1805e5428db8b736a65d095
Maritta Gostanian <maritta886@gmail.com>
2025-10-21 23:21:30 -0400

    Implement dialog and hamburger menu hide functions
    Added functions to hide dialog and hamburger menu.

--- sudoku.js
+++ sudoku.js
@@ -407,72 +407,24 @@ function moreOptionButtonClick() {
    }, 50);
 }
 
+function hideDialogButtonClick(dialogId) {
+   var dialog = document.getElementById(dialogId);
+   var dialogBox = document.getElementById(dialogId + "-box");
+   if (!dialog || !dialogBox) return;
+   dialog.style.opacity = 0;
+   dialogBox.style.marginTop = "-500px";
+   setTimeout(function(){ dialog.style.visibility = "collapse"; dialog.style.display = "none"; }, 500);
+}
 
+function hideHamburgerClick() {
+   var div = document.getElementById("hamburger-menu");
+   var menu = document.getElementById("nav-menu");
+   if (!div || !menu) return;
+   menu.style.left = "-256px";
+   setTimeout(function(){ div.style.opacity = 0; div.style.visibility = "collapse"; div.style.display = "none"; }, 200);
+}
 
    
-      this.appendChild(rippleItem);
-      // set timer to remove the dif after the effect ends 
-      setTimeout(function () {rippleItem.parentElement.removeChild(rippleItem);}, 1500);};
-  }
-  for (var i = 0; i < 9; i++) {
-    for (var j = 0; j < 9; j++) {
-      var input = table.rows[i].cells[j].getElementsByTagName("input")[0];
-      // add function to remove color from cells and update remaining table when it get changed 
-      input.onchange = function () {
-        //remove color from cell 
-        addClassToCell(this);
-        // check if the new value entered is allowed 
-        function checkInput(input) {
-          if (input.value[0] < "1" || input.value[0] > "9") {
-            if (input.value != "?" && input.value != "؟") {
-              input.value = "";
-              alert("only numbers [1-9] and question mark '?' are allowed!!");
-              input.focus();
-            }
-          }
-        }
-        checkInput(this);
-        // compare old value and new value then update remaining table 
-        if (this.value > 0 && this.value < 10) {
-          remaining[this.value - 1]--;
-        }
-        if (this.oldvalue !== "") {
-          if (this.oldvalue > 0 && this.oldvalue < 10) {
-            remaining[this.oldvalue - 1]++;
-          }
-        }
-        //reset canSolved value when change any cell 
-        canSolved = true;
-        updateRemainingTable();};
-      //change cell 'old value' when it got focused to track numbers and changes on grid 
-      input.onfocus = function () {this.oldvalue = this.value;};
-    }
-  }
-};
-
-// function to hide dialog opened in window 
-window.onclick = function (event) {
-  var d1 = document.getElementById("dialog");
-  var d2 = document.getElementById("about-dialog");
-  var m1 = document.getElementById("more-option-list");
-  if (event.target == d1) {
-    hideDialogButtonClick("dialog");
-  } else if (event.target == d2) {
-    hideDialogButtonClick("about-dialog");
-  } else if (m1.style.visibility == "visible") {
-    hideMoreOptionMenu();
-  }
-};
-
-// show hamburger menu 
-function HamburgerButtonClick() {
-  debugger
-  var div = document.getElementById("hamburger-menu");
-  var menu = document.getElementById("nav-menu");
-  div.style.display = "block";
-  div.style.visibility = "visible";
-  setTimeout(function() {div.style.opacity = 1; menu.style.left = 0;}, 50);
-}
 
 // start new game 
 function startGameButtonClick() {
